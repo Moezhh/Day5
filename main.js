313da9643404b8cc8fbfd4147baa81ca78ab4ec3
moezhh <moezhadjhamida@gmail.com>
2020-08-18 19:57:13 +0100

    update website day5

---
+++ main.js
@@ -0,0 +1,15 @@
+var i, x, y = [], espace = ' ', nbretage = prompt("Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?") ;
+i = 0 ; x = '' ;
+
+     do 
+         {
+         espace = " ".repeat(12-i);	
+	     x = x + "#";
+         i = i + 1 ;
+         y[i]= espace + x + "\n" ;
+         }
+        while ( i <  nbretage );
+    if ( nbretage < 26 && nbretage > 0 )
+         alert( 'Voici la pyramide :' + "\n" + y );
+  
+
